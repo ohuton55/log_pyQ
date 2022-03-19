a5be74b42f2ae65505d4632c2b2401f09f8d2b96
ohuton55 <tttt.tttt.no@gmail.com>
2022-03-19 12:41:19 +0900

    I tried to read the character string.

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello, world!")
+import os
+
+os.chdir(r'C:\Users\hntrk\log_pyQ')
+
+print(os.getcwd())
+
+f = open('sample_text.txt', 'r', encoding='utf-8')
+sampleTxts = f.readlines()
+f.close
+
+for sampleTxt in sampleTxts:
+	sampleTxt_split = sampleTxt.split()
+
+	print(sampleTxt_split)
