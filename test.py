24b9492e34d4735b17fed962bedc871d6a283493
ohuton55 <tttt.tttt.no@gmail.com>
2022-05-31 20:53:16 +0900

    test commit!

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ import sys
 #カレントディレクトリを変更
 os.chdir(r'C:\Users\hntrk\github\log_pyQ')
 
-#printの出力結果をUTF-8に
+#printの出力結果をUTF-8にP
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
-#print(os.getcwd())
-#print ('defaultencoding:', sys.getdefaultencoding())
+print(os.getcwd())
+print ('defaultencoding:', sys.getdefaultencoding())
 
 f = open('sample_text.txt', 'r', encoding='utf-8')
 sampleTxts = f.readlines()
