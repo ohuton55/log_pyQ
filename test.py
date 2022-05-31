22ebc577cf40729178c83f452f17d7dcf0bf0fda
ohuton55 <tttt.tttt.no@gmail.com>
2022-05-31 20:44:59 +0900

    Change the character code and open the file.

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import os
+import io
+import sys
 
-os.chdir(r'C:\Users\hntrk\log_pyQ')
+#カレントディレクトリを変更
+os.chdir(r'C:\Users\hntrk\github\log_pyQ')
 
-print(os.getcwd())
+#printの出力結果をUTF-8に
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
+#print(os.getcwd())
+#print ('defaultencoding:', sys.getdefaultencoding())
 
 f = open('sample_text.txt', 'r', encoding='utf-8')
 sampleTxts = f.readlines()
@@ -11,4 +21,4 @@ f.close
 for sampleTxt in sampleTxts:
 	sampleTxt_split = sampleTxt.split()
 
-	print(sampleTxt_split)
+	print(sampleTxt_split, file=sys.stderr)
